7341f5801b9cb97dd7b2a1ca35c70ff4f81f5430
Ramesh <ramesh.kec.85@gmail.com>
2016-04-04 15:40:29 +0530

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+Inspire from :
+http://jeroenmols.com/blog/2015/08/13/artifactory2/
---
+++ app/src/main/java/com/andhradroid/testartifactory/CalculatorLibrary.java
@@ -0,0 +1,19 @@
+package com.andhradroid.testartifactory;
+
+/**
+ * Created by Ramesh on 4/4/16.
+ */
+public class CalculatorLibrary {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int substraction(int x, int y) {
+        return x - y;
+    }
+}
